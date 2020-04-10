8094503f45b7e35eba590dadb250a2a33655006c
Paweł Skalny <skalny@student.agh.edu.pl>
2020-04-10 10:34:03 +0200

    [KASJ-48/49] Refactoring names

--- getdata.py
+++ getdata.py
@@ -6,8 +6,7 @@ try:
 except ImportError:
     raise ImportError('PyMongo is not installed')
 
-
-class MongoDB(object):
+class MongoDBClient(object):
     def __init__(self, host='localhost', port=27017, database_name=None, collection_name=None):
         try:
             self._connection = MongoClient(host=host, port=port, maxPoolSize=200)
@@ -24,53 +23,54 @@ class MongoDB(object):
         post_id = self._collection.insert_one(post).inserted_id
         return post_id
 
-wydzialy = ['wgig', 'wimiip', 'weaiiib', 'wieit', 'wimir', 'wggios', 'wggiis', 'wimic', 'wo', 'wmn', 'wwnig', 'wz', 'wms', 'wh', 'wfiis']
+departments = ['wgig', 'wimiip', 'weaiiib', 'wieit', 'wimir', 'wggios', 'wggiis', 'wimic', 'wo', 'wmn', 'wwnig', 'wz', 'wms', 'wh', 'wfiis']
 
 urlBase = 'https://syllabuskrk.agh.edu.pl/current_annual/magnesite/api/faculties/'
 
-kierunkiDoPob = {}
+coursesForDownload = {}
 
-w = wydzialy[len(wydzialy) - 1]
+w = departments[len(departments) - 1]
 url = urlBase + '{}/study_plans'.format(w)
 
-response = requests.get(url)
+headers = {'Accept-Language': 'pl'}
+response = requests.get(url, headers=headers)
 data = json.loads(response.text) if response.text is not None else None
 
 if data is not None:
     for t in data['syllabus']['study_types']:
         levelsName = []
         for l in t['levels']:
-            kierName = []
+            courseName = []
             for s in l['study_programmes']:
                 name = s['url'].split('/')
                 nameMain = name[len(name) - 1]
-                kierName.append({'name': nameMain, 'shortName': s['name']})
-            levelsName.append({'stopien': l['level'], 'kierunki': kierName})
-        kierunkiDoPob[w] = levelsName
+                courseName.append({'name': nameMain, 'shortName': s['name']})
+            levelsName.append({'level': l['level'], 'courseName': courseName})
+        coursesForDownload[w] = levelsName
 
-przedmiotyNaKier = []
+subjectForCourse = []
 
-for key, item in kierunkiDoPob.items():
-    for i,stopnie in enumerate(item):
-        for j,k in enumerate(stopnie['kierunki']):
+for key, item in coursesForDownload.items():
+    for i,singleLevel in enumerate(item):
+        for j,k in enumerate(singleLevel['courseName']):
             urlP = urlBase + '{}/study_plans/{}'.format(key, k['name'])
-            responseP = requests.get(urlP)
+            responseP = requests.get(urlP, headers=headers)
             dataP = json.loads(responseP.text) if responseP.text is not None else None
             for sem in dataP['syllabus']['study_plan']['semesters']:
                 for g in sem['groups']:
                     if 'modules' in g.keys():
                         for m in g['modules']:
-                            przedmiotyNaKier.append({'name': m['name'], 'ects': m['ects_credits'],
-                                'module': k['shortName'], 'semester': sem['number'], 
-                                'hours': {h['name']:h['classes_hours'] for h in m['form_of_classes']}})
+                            subjectForCourse.append({'name': m['name'], 'ects': m['ects_credits'],
+                                'fieldOfStudy': k['shortName'], 'semester': sem['number'], 
+                                'hours': {h['name'].lower():h['classes_hours'] for h in m['form_of_classes']}})
                     elif 'groups' in g.keys():
                         for go in g['groups']:
                             for mo in go['modules']:
                                 print(mo)
 
 print('[*] Pushing data to MongoDB ')
-mongo_db = MongoDB(database_name='KasjopejaDB', collection_name=w)
+mongodbClient = MongoDBClient(database_name='KasjopejaDB', collection_name=w)
 
-for collection in przedmiotyNaKier:
+for collection in subjectForCourse:
     print('[!] Inserting - ', collection)
-    mongo_db.insert(collection)
+    mongodbClient.insert(collection)
