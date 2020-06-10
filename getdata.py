a3dcede5cd2d841301d74b4f1c23c93ff2dd8dd5
Paweł Skalny <skalny@student.agh.edu.pl>
2020-06-10 20:58:04 +0200

    KASJ - Added academic year to data structure

--- getdata.py
+++ getdata.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import sys
+import datetime
 
 if len(sys.argv) < 5:
     print("Provide MongoDB connection parameters -> hostAddress port databaseName collectionName")
@@ -35,64 +36,72 @@ print("[*] Preparing data for MongoDB")
 
 departments = ['wgig', 'wimiip', 'weaiiib', 'wieit', 'wimir', 'wggios', 'wggiis', 'wimic', 'wo', 'wmn', 'wwnig', 'wz', 'wms', 'wh', 'wfiis']
 
-urlBase = 'https://syllabuskrk.agh.edu.pl/current_annual/magnesite/api/faculties/'
+now = datetime.datetime.now()
+academicYears = [f'{str(x)}-{str(x+1)}' for x in range(2012, int(now.year))]
 
-coursesForDownload = {}
+for year in academicYears:
+    urlBase = f'https://syllabuskrk.agh.edu.pl/{year}/magnesite/api/faculties/'
 
-w = departments[len(departments) - 1]
-url = urlBase + '{}/study_plans'.format(w)
+    coursesForDownload = {}
 
-headers = {'Accept-Language': 'pl'}
-response = requests.get(url, headers=headers)
-data = json.loads(response.text) if response.text is not None else None
+    w = departments[len(departments) - 1]
+    url = urlBase + '{}/study_plans'.format(w)
 
-if data is not None:
-    for t in data['syllabus']['study_types']:
-        levelsName = []
-        for l in t['levels']:
-            courseName = []
-            for s in l['study_programmes']:
-                name = s['url'].split('/')
-                nameMain = name[len(name) - 1]
-                courseName.append({'name': nameMain, 'shortName': s['name']})
-            levelsName.append({'level': l['level'], 'courseName': courseName})
-        coursesForDownload[w] = levelsName
+    headers = {'Accept-Language': 'pl'}
+    response = requests.get(url, headers=headers)
+    data = json.loads(response.text) if response.text is not None else None
 
-subjectForCourse = []
+    if data is not None:
+        for t in data['syllabus']['study_types']:
+            levelsName = []
+            for l in t['levels']:
+                courseName = []
+                for s in l['study_programmes']:
+                    name = s['url'].split('/')
+                    nameMain = name[len(name) - 1]
+                    courseName.append({'name': nameMain, 'shortName': s['name']})
+                    print()
+                levelsName.append({'level': l['level'], 'courseName': courseName})
+            coursesForDownload[w] = levelsName
 
-for key, item in coursesForDownload.items():
-    for i,singleLevel in enumerate(item):
-        for j,k in enumerate(singleLevel['courseName']):
-            urlP = urlBase + '{}/study_plans/{}'.format(key, k['name'])
-            responseP = requests.get(urlP, headers=headers)
-            dataP = json.loads(responseP.text) if responseP.text is not None else None
-            lSem = len(dataP['syllabus']['study_plan']['semesters'])
-            for semi, sem in enumerate(dataP['syllabus']['study_plan']['semesters']):
-                lGroup = len(sem['groups'])
-                for gi, g in enumerate(sem['groups']):
-                    lG = len(g['groups']) if 'groups' in g.keys() else 0
-                    if 'modules' in g.keys():
-                        lMod = len(g['modules'])
-                        for mi, m in enumerate(g['modules']):
-                            subjectForCourse.append({'name': m['name'], 'ects': m['ects_credits'],
-                                'fieldOfStudy': k['shortName'], 'semester': sem['number'],
-                                'hours': {h['name'].lower():h['classes_hours'] for h in m['form_of_classes']}})
-                    elif 'groups' in g.keys():
-                        for go in g['groups']:
-                            for mo in go['modules']:
-                                #print(mo)
-                                pass
-else:
-    print("Done!")
+    subjectForCourse = []
 
-print('[*] Peparing client form MongoDB ')
-mongodbClient = MongoDBClient(host=sys.argv[1], port=int(sys.argv[2]), database_name=sys.argv[3], collection_name=sys.argv[4])
+    for key, item in coursesForDownload.items():
+        for i,singleLevel in enumerate(item):
+            for j,k in enumerate(singleLevel['courseName']):
+                urlP = urlBase + '{}/study_plans/{}'.format(key, k['name'])
+                print(urlP)
+                sys.exit()
+                responseP = requests.get(urlP, headers=headers)
+                dataP = json.loads(responseP.text) if responseP.text is not None else None
+                lSem = len(dataP['syllabus']['study_plan']['semesters'])
+                for semi, sem in enumerate(dataP['syllabus']['study_plan']['semesters']):
+                    lGroup = len(sem['groups'])
+                    for gi, g in enumerate(sem['groups']):
+                        lG = len(g['groups']) if 'groups' in g.keys() else 0
+                        if 'modules' in g.keys():
+                            lMod = len(g['modules'])
+                            for mi, m in enumerate(g['modules']):
+                                subjectForCourse.append({'name': m['name'], 'ects': m['ects_credits'],
+                                    'fieldOfStudy': k['shortName'], 'semester': sem['number'],
+                                    'academicYear': year,
+                                    'hours': {h['name'].lower():h['classes_hours'] for h in m['form_of_classes']}})
+                        elif 'groups' in g.keys():
+                            for go in g['groups']:
+                                for mo in go['modules']:
+                                    #print(mo)
+                                    pass
+    else:
+        print("Done!")
 
-print('[!] Clearing old data from collection: ', sys.argv[4])
-mongodbClient.clean()
+    print('[*] Peparing client form MongoDB ')
+    mongodbClient = MongoDBClient(host=sys.argv[1], port=int(sys.argv[2]), database_name=sys.argv[3], collection_name=sys.argv[4])
 
-for i, collection in enumerate(subjectForCourse):
-    print("[!] Inserting - %.2f%s done" % (i/len(subjectForCourse)*100, "%"))
-    mongodbClient.insert(collection)
-else:
-    print("100% Done!")
+    print('[!] Clearing old data from collection: ', sys.argv[4])
+    mongodbClient.clean()
+
+    for i, collection in enumerate(subjectForCourse):
+        print("[!] Inserting - %.2f%s done" % (i/len(subjectForCourse)*100, "%"))
+        mongodbClient.insert(collection)
+    else:
+        print("100% Done!")
